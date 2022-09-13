2738e1f7bfbaf1442c282db06397490ea6d683d6
Steph DGenius <stephanetse@gmail.com>
2022-09-13 05:06:06 +0100

    0.1.0-alpha.02

--- gh-build.sh
+++ gh-build.sh
@@ -4,8 +4,6 @@ ARCH=$1
 VERSION=13092022
 GIT_CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-OUTDIR_DIR=$PWD/ouputs
-
 # Create new directory for this work
 
 BASE_SYSTEM=rootfs_$ARCH_$GIT_CURRENT_BRANCH
@@ -36,7 +34,5 @@ echo "Compressing $BASE_SYSTEM with XZ (using $(nproc) threads)..."
 xz -v --threads=$(nproc) "$BASE_SYSTEM.tar"
 
 # Moving generated archive
-
-mv $BASE_SYSTEM.tar.xz "$OUTDIR_DIR/$BASE_SYSTEM.tar.xz"
 
 echo "Successfully created $BASE_SYSTEM.xz."
